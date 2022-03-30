92ab710fca5cef57fa8e4bbaa3f7ba7194ca2d6c
hkhodr <31921730+hkhodr@users.noreply.github.com>
2022-03-30 16:00:15 +0200

    update

--- scripts.js
+++ scripts.js
@@ -3,12 +3,15 @@ const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sa
 const possibledays = ["Monday","Tuesday","Thursday"]
 
 const veggyday= new Date('2022-03-22');
+veggyday.setHours(0,0,0);
 const today = new Date();
 const tomorrow = new Date();
 tomorrow.setDate(today.getDate()+1);
+tomorrow.setHours(0,0,0);
 
 let day = weekday[tomorrow.getDay()];
-var days_difference = tomorrow.getDate() - veggyday.getDate();   
+var time_difference = tomorrow.getTime() - veggyday.getTime();   
+var days_difference = Math.ceil(time_difference / (1000 * 60 * 60 * 24));  
 
 if(!possibledays.includes(day))
     document.getElementById('answer').innerHTML = "NO";
@@ -22,4 +25,3 @@ else{
 }
 
 document.getElementById('date').innerHTML = tomorrow;
-
